5d0934dbc97fb654e2a36e169049ccc6b2a84a2c
Joshua <joshuadstarkey@gmail.com>
2018-10-17 01:22:07 -0700

    some psudo code, container added

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// create countdown timer that counts down from 30seconds
+// create if statements around counter running out
+// create an object that holds questions and answers
+// Create win/lose variables
