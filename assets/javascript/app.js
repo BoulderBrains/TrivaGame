1c9c4a65ccfc477b3d7bb3ceeb3f8d4e6bb029bd
Joshua <joshuadstarkey@gmail.com>
2018-10-19 16:46:15 -0700

    Working on the trivia game start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,18 @@
 // create an object that holds questions and answers
 // Create win/lose variables
 // create correct/incorrect variables
+
+$(document).ready(function() {
+	//When the page is ready, trigger the startScreen
+	function startWindow() {
+		startWindow = "<h3>Ready for a Trivia challenge?</h3><a class='btn btn-primary' id='startButton' href='#'>Start</a>";
+		$(".body-container").html(startWindow);
+	}
+
+	startWindow();
+
+	// On click of the start-button, run the following code
+	$("body").on("click", "#startButton", function(event){
+
+	});
+})
